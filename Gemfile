184757a83e9afdfd0d0327ed1f12cace7e11712d
Ben Kimball <ben.kimball@youearnedit.com>
2018-07-18 16:31:21 -0500

    Remove pg, certbot challenge

--- Gemfile
+++ Gemfile
@@ -3,7 +3,3 @@ ruby '~> 2.5.1'
 
 gem 'sinatra'
 gem 'thin'
-gem 'pg'
-# gem 'activerecord'
-# gem 'sinatra-activerecord'
-# gem 'rake'
--- app.rb
+++ app.rb
@@ -1,10 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  conn = PG::Connection.new
-  "hello #{conn.db}"
-end
-
-get '/.well-known/acme-challenge/VEQxTMpdijgKUPzrDFW5eInPq_aKljanRUCHdS8Y5JA' do
-  ENV['CERTBOT_CHALLENGE']
+  'hello from Sinatra'
 end
